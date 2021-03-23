bacd1a2be99b6051909c52d814e72f115aa19df6
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-23 14:36:56 +0100

    add style

--- js/script.js
+++ js/script.js
@@ -6,15 +6,19 @@
     3. Stampa Fizz per i divisibili per 3, Buzz per i divisibili per 5, FizzBuzz per i divisibili per entrambi
     4. Per tutti gli altri casi stampa il numero
  */
-
-    for(i = 1; i <= 100; i++){
-        if((i % 3 == 0) && (i % 5 == 0)){
-            console.log('FizzBuzz');
-        } else if(i % 3 == 0){
-            console.log('Fizz');
-        } else if(i % 5 == 0){
-            console.log('Buzz');
-        } else{
-            console.log(i);
-        }
+var limit = 100;
+for(i = 1; i <= limit; i++){
+    if((i % 3 == 0) && (i % 5 == 0)){
+        // console.log('FizzBuzz');
+        document.getElementById(i).innerHTML = '<span class="fizz-buzz">FizzBuzz</span>';
+    } else if(i % 3 == 0){
+        // console.log('Fizz');
+        document.getElementById(i).innerHTML = '<span class="fizz">Fizz</span>';
+    } else if(i % 5 == 0){
+        // console.log('Buzz');
+        document.getElementById(i).innerHTML = '<span class="buzz">Buzz</span>';
+    } else{
+        // console.log(i);
+        document.getElementById(i).innerHTML = i;
     }
+}
